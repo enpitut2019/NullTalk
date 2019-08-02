4e7088fdb520973704b0560aa10545a5eb0fb6c4
Pelkira <p.true.next@gmail.com>
2019-08-02 13:35:38 +0900

    addList.php : insert処理を追加

---
+++ addList.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>ぬるトーク</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <style type='text/css'>
+    body {
+      background-color: #ddd;
+      color: #333333;
+      e-height: 1;
+    }
+  </style>
+</head>
+
+<body>
+  <h1 style="font-size: 80px ">ぬるトークへようこそ</h1>
+  <input type="submit" name="add_list" value="趣味を追加">
+</body>
+
+<?php
+    $url = parse_url(getenv("DATABASE_URL"));
+    $con = pg_connect("host=" . $url['host'] . " port=" 
+     . $url['port'] . " dbname=" . substr($url['path'], 1)
+     . " user=" . $url['user'] . " password=" . $url['pass']);
+
+    if (isset($_POST["add_list"])) {
+        $res = pg_query($con, "INSERT INTO hobbies VALUES(1, 'ゲーム')") or die("クエリ実行エラーです" . pg_last_error());
+    }
+?>
+
+</html>
