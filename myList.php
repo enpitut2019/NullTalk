aa2551f17c9d1b2c3687b2eccf4c8cb63bbf05c2
Pelkira <p.true.next@gmail.com>
2019-08-02 15:10:45 +0900

    for test5

--- myList.php
+++ myList.php
@@ -28,10 +28,9 @@
     <ul>
     <?php
     $res = pg_query($con, "SELECT hobby FROM hobbies WHERE uid=1") or die("クエリ実行エラーです" . pg_last_error());
-    print_r($res);
     while($hobby = pg_fetch_row($res)) {
         echo '<li><a style="font-size: 80px ">';
-        echo $hobby;
+        echo $hobby[0];
         echo '</a></li>';
     }
     ?>
