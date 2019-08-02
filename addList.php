aa4995dc81ffe12e8911dcc49dd3cb83d73acf81
UmezuPenguin <ayoanuzemu.work.2240@gmail.com>
2019-08-02 16:33:04 +0900

    traced chkBox sample

--- addList.php
+++ addList.php
@@ -18,11 +18,26 @@
   <h1 style="font-size: 80px ">追加する趣味を選んでください</h1>
   <form action="addListResult.php" method="post">
     <div class="addHobbyList">
-      <label><input type="checkbox" name="hobbyAdd" value="料理">料理</input><br /></label>
-      <label><input type="checkbox" name="hobbyAdd" value="料理2">料理2</input><br /></label>
-      <label><input type="checkbox" name="hobbyAdd" value="料理3">料理3</input><br /></label>
-      <label><input type="checkbox" name="hobbyAdd" value="料理4">料理4</input><br /></label>
-      <label><input type="checkbox" name="hobbyAdd" value="料理5">料理5</input><br /></label>
+      <div class="chkbox">
+	      <input type="checkbox" id="chkbox1" name="chkbox1" value="0" />
+	      <label for="chkbox1">料理1</label>
+      </div>
+      <div class="chkbox">
+	      <input type="checkbox" id="chkbox2" name="chkbox2" value="0" />
+	      <label for="chkbox2">料理2</label>
+      </div>
+      <div class="chkbox">
+	      <input type="checkbox" id="chkbox3" name="chkbox3" value="0" />
+	      <label for="chkbox3">料理3</label>
+      </div>
+      <div class="chkbox">
+	      <input type="checkbox" id="chkbox4" name="chkbox4" value="0" />
+	      <label for="chkbox4">料理4</label>
+      </div>
+      <div class="chkbox">
+	      <input type="checkbox" id="chkbox5" name="chkbox5" value="0" />
+	      <label for="chkbox5">料理5</label>
+      </div>
     </div>
     <input class="addButton" type="submit" name="addList" value="追加">
   </form>
