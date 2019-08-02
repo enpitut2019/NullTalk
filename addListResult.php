6f54a07114f77e9719ff4811ae7075a744fee4b0
Pelkira <p.true.next@gmail.com>
2019-08-02 14:20:16 +0900

    add addListResult

---
+++ addListResult.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>ぬるトーク</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <style type='text/css'>
+    body {
+      background-color: #ddd;
+      color: #333333;
+      e-height: 1;
+    }
+  </style>
+</head>
+
+<body>
+  <h1 style="font-size: 80px ">ぬるトークへようこそ</h1>
+  <form action="addList.php" method="post">
+    <input type="submit" name="add_list" value="趣味を追加">
+  </form>
+  <?php
+    $url = parse_url(getenv("DATABASE_URL"));
+    $con = pg_connect("host=" . $url['host'] . " port=" 
+     . $url['port'] . " dbname=" . substr($url['path'], 1)
+     . " user=" . $url['user'] . " password=" . $url['pass']);
+    if (isset($_POST["add_list"])) {
+        $res = pg_query($con, "INSERT INTO hobbies VALUES(1, 'ゲーム')") or die("クエリ実行エラーです" . pg_last_error());
+        print($res);
+    }
+  ?>
+</body>
+
+</html>
