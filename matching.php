be1b43bda9d86048d74087f433be58cc4b04e1c3
Pelkira <p.true.next@gmail.com>
2019-08-05 14:32:22 +0900

    create a matching_php

--- matching.php
+++ matching.php
@@ -1,19 +1,4 @@
-<?php
-    // セッション開始！
-    $check = session_start();
 
-    if(!isset($check)){
-        print("セッションの確立に失敗しました");
-      }
-      $session_id = session_id();
-      $url = parse_url(getenv("DATABASE_URL"));
-      $con = pg_connect("host=" . $url['host'] . " port=" 
-      . $url['port'] . " dbname=" . substr($url['path'], 1)
-      . " user=" . $url['user'] . " password=" . $url['pass']);
-    
-      $res = pg_query($con, "SELECT uid FROM users WHERE session_id = '".$session_id."'"); #or die("クエリ実行エラーです" . pg_last_error());
-            
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,8 +23,10 @@
 <body>
     <h1>友達を指定します</h1>
     <p>合言葉を入力してください</p>
-    <input type="text" name="name" size="30" maxlength="4">
-    <button type="button" onclick="location.href='result.html'" name="result">GO!</button>
+    <form action="waiting.php" method="post">
+        <input type="text" name="password" size="30" maxlength="4">
+        <input class="addButton" type="submit" name="addList" value="GO!">
+    </form>
 </body>
 
 </html>
