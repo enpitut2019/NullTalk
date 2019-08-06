feeb9cd944e668535c1b044bdd5c2e46c029b3ef
Pelkira <p.true.next@gmail.com>
2019-08-06 10:55:13 +0900

    delete print

--- addListResult.php
+++ addListResult.php
@@ -39,18 +39,15 @@
     $session_id = session_id();
         
     foreach($_POST["hobbyAdd"] as $hobby_name){
-        print('hobby_name '.$hobby_name);
         if (isset($hobby_name)) {
             $res_uid = pg_query($con, "SELECT uid FROM users WHERE session_id = '".$session_id."'"); #or die("クエリ実行エラーです" . pg_last_error());
             $uid = pg_fetch_row($res_uid)[0];
-            print('uid '.$uid);
 
             
             $res_hid = pg_query($con, "SELECT hid FROM hobbies WHERE hobby_name = '".$hobby_name."'"); # or die("クエリ実行エラーです" . pg_last_error());
             $hid = pg_fetch_row($res_hid)[0];
-            print('hid '.$hid);
 
-            $res_ins = pg_query($con, "INSERT INTO user_hobbies VALUES(".$uid.", ".$hid.")") or die("クエリ実行エラーです" . pg_last_error());
+            $res_ins = pg_query($con, "INSERT INTO user_hobbies VALUES(".$uid.", ".$hid.")"); # or die("クエリ実行エラーです" . pg_last_error());
             #print($res);
         }
     }
