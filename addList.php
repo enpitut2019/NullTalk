666ed62c9096cb1b72b4a5ec50d08cb16f6eb5e0
Pelkira <p.true.next@gmail.com>
2019-08-06 15:06:21 +0900

    small font for addList.php

--- addList.php
+++ addList.php
@@ -24,7 +24,7 @@
     </ul>
   </div>
   <div id="main">
-  <h1 style="font-size: 80px ">追加する趣味を選んでください</h1>
+  <h1 style="font-size: 20px ">追加する趣味を選んでください</h1>
   <form action="addListResult.php" method="post">
     <div class="addHobbyList">
       <label><input type="checkbox" name="hobbyAdd[]" value="ゴルフ">ゴルフ</input><br /></label>
