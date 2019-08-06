f609aa75b48f3d1e72eb9b5b99878f2f43fd9ed7
Pelkira <p.true.next@gmail.com>
2019-08-06 14:31:25 +0900

    modified myList.php

--- myList.php
+++ myList.php
@@ -5,47 +5,54 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>ぬるトーク</title>
   <link rel="stylesheet" type="text/css" href="main.css">
-  <style type='text/css'>
-    body {
-      background-color: #ddd;
-      color: #333333;
-      e-height: 1;
-    }
-  </style>
 </head>
 
         
 
 <body>
-  <h1>あなたの趣味はこちらです</h1>
-  <div class="listCenter">
-    <ul>
-      <?php
-        $check = session_start();
-        if(!isset($check)){
-          print("セッションの確立に失敗しました");
-        }
-        $session_id = session_id();
-
-        $url = parse_url(getenv("DATABASE_URL"));
-        $con = pg_connect("host=" . $url['host'] . " port=" 
-          . $url['port'] . " dbname=" . substr($url['path'], 1)
-          . " user=" . $url['user'] . " password=" . $url['pass']);
-
-        $res_uid = pg_query($con, "SELECT uid FROM users WHERE session_id = '".$session_id."'"); #or die("クエリ実行エラーです" . pg_last_error());
-        $uid = pg_fetch_row($res_uid)[0];
-
-        $res_hids = pg_query($con, "SELECT hid FROM user_hobbies WHERE uid = '".$uid."'"); # or die("クエリ実行エラーです" . pg_last_error());
-        while($hid = pg_fetch_row($res_hids)) {
-          $hobby_name = pg_query($con, "SELECT hobby_name FROM hobbies WHERE hid=".$hid[0]); 
-          echo '<li><a style="font-size: 80px ">';
-          echo pg_fetch_row($hobby_name)[0];
-          echo '</a></li>';
-        }
-      ?>
-      
+  <div id="header">
+    <h1>NullTalk</h1>
+    <ul id="menu">
+      <li><a href="index.php">ホーム</a></li>
+      <li><a href="myList.php">趣味の閲覧</a></li>
+      <li><a href="addList.php">趣味の追加</a></li>
+      <li><a href="matching.php">マッチング</a></li>
     </ul>
-    <a href="index.php">戻る</a>
+  </div>
+  <div id="main">
+    <h1>あなたの趣味はこちらです</h1>
+    <div class="listCenter">
+      <ul>
+        <?php
+          $check = session_start();
+          if(!isset($check)){
+            print("セッションの確立に失敗しました");
+          }
+          $session_id = session_id();
+
+          $url = parse_url(getenv("DATABASE_URL"));
+          $con = pg_connect("host=" . $url['host'] . " port=" 
+            . $url['port'] . " dbname=" . substr($url['path'], 1)
+            . " user=" . $url['user'] . " password=" . $url['pass']);
+
+          $res_uid = pg_query($con, "SELECT uid FROM users WHERE session_id = '".$session_id."'"); #or die("クエリ実行エラーです" . pg_last_error());
+          $uid = pg_fetch_row($res_uid)[0];
+
+          $res_hids = pg_query($con, "SELECT hid FROM user_hobbies WHERE uid = '".$uid."'"); # or die("クエリ実行エラーです" . pg_last_error());
+          while($hid = pg_fetch_row($res_hids)) {
+            $hobby_name = pg_query($con, "SELECT hobby_name FROM hobbies WHERE hid=".$hid[0]); 
+            echo '<li><a style="font-size: 80px ">';
+            echo pg_fetch_row($hobby_name)[0];
+            echo '</a></li>';
+          }
+        ?>
+        
+      </ul>
+      <a href="index.php">戻る</a>
+    </div>
+  </div>
+  <div id="footer">
+    <span id="footer_text">Copyright&copy; 2019 ぬるんちゅ All Rights Reserved.</span>
   </div>
 </body>
 
